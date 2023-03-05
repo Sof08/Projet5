b77073a5e24986865dbf6ea2d24c276a0575969e
Sof08 <114166049+Sof08@users.noreply.github.com>
2023-03-05 22:40:04 +0100

    Update confirmation.js

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,5 +1,5 @@
 //Récuperer les infos des parametres de l'URL
-let idCommande = new URL(location.href).searchParams.get("orderId"); //récupère l'id contenu dans l'url de la page actuelle
+let idCommande = new URL(location.href).searchParams.get("orderId"); 
 
 //afficher l'id dans le span id 
 document.querySelector('#orderId').textContent = idCommande;
